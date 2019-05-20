87d4890d0bc55f58827328d7b63fe5a43d6427ac
Nyesha Jones <neeshajolmfao@gmail.com>
2019-05-20 22:21:17 +0000

    I mean.....

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-var custom_api_url = "https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+var custom_api_url = "https://api.soundcloud.com/tracks?q=" + "searchTerm" + "&client_id=5aa8e389ba4e24b6106af5159ab3e344";
 // var api_key = ""
 console.log(custom_api_url)
 
-$("#search-button").click(function(){
-    var searchTerm = $("#search-term").val();
+$("#searchButton").click(function() {
+    var searchTerm = $("#searchTerm").val();
     $.ajax({
-        url: custom_api_url,// + searchTerm + api_key,
+        url: custom_api_url,
         method: "GET",
         success: function(response) {
-            console.log(response)
             
-           // $(".gallery").html("<img src='" + response.data[4].images.original.url + "'>" );
+        var songTitle= response[0].title;
+        var songDes = response[0].user;
+        //console.log(songTitle)
+        //console.log(songDes)
+        
+         //var imgUrl = response[0].artwork_url;
+        //var songPic = <img src = ' + imgUrl +' >
+        
             
+           //(".gallery").html("<img src='" + response.data[4].images.original.url + "'>" );
+            
+          $("body").append(songTitle)
+          //$("body").append(songDes)
         }
     });
 });
